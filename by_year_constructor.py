f3c034e63c03afbb78e03b47243fb998cb79969b
timpel <timpellissier@gmail.com>
2017-12-31 23:36:23 -0600

    implement uid-key dict

--- by_year_constructor.py
+++ by_year_constructor.py
@@ -4,23 +4,72 @@ import sys
 import json
 from dateutil.parser import *
 
+def get_year(date_str):
+	try:
+		yr = parse(date_str).year
+		return yr
+	except ValueError:
+		return None
 
-def by_year(obj):
-	people_by_year = {}
+def by_uid(obj):
+	people = {}
+	fields_to_keep = ['_UID', 'NAME']
+
 	for person in obj:
+		info = {
+			'_UID': None,
+			'NAME': None,
+			'BLAT': None,
+			'BLON': None,
+			'DLAT': None,
+			'DLON': None,
+			'B_YR': None,
+			'D_YR': None
+		}
 		for person_field in person['tree']:
-			if person_field
+			tag = person_field['tag']
+			data = person_field['data']
+			if tag in fields_to_keep:
+				info[tag] = data
+			if tag == 'BIRT':
+				for birth_field in person_field['tree']:
+					tag = birth_field['tag']
+					data = birth_field['data']
+					if tag == 'LATI':
+						info['BLAT'] = float(data)
+					if tag == 'LONG':
+						info['BLON'] = float(data)
+					if tag == 'DATE':
+						info['B_YR'] = get_year(data)
+			if tag == 'DEAT':
+				for death_field in person_field['tree']:
+					tag = death_field['tag']
+					data = death_field['data']
+					if tag == 'LATI':
+						info['DLAT'] = float(data)
+					if tag == 'LONG':
+						info['DLON'] = float(data)
+					if tag == 'DATE':
+						info['D_YR'] = get_year(data)
+
+		people[info['_UID']] = info
+
+	return people
 
 def main():
 	# Verify that 1 parameter was entered in command line
-	if len(sys.argv) != 2:
-		print 'Usage: python by_year_constructor.py input_file'
+	if len(sys.argv) != 3:
+		print 'Usage: python by_year_constructor.py input_file output_file'
 	
 	else:
 		with open(sys.argv[1]) as contactFile:
 		    data = json.load(contactFile)
 
-		result = by_year(data)
+		uids = by_uid(data)
+
+		file = open(sys.argv[2], "w")
+
+		print >> file, json.dumps(uids)
 		
 
 # If scraper.py was run directly by python
